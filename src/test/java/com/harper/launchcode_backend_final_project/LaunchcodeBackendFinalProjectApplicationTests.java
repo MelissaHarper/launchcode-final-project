b960545cc4edd92ab6bf929071bb2e9df652c205
Harper <HarperMelissaM@gmail.com>
2025-07-10 15:47:37 -0500

    init commit

---
+++ src/test/java/com/harper/launchcode_backend_final_project/LaunchcodeBackendFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harper.launchcode_backend_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchcodeBackendFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
