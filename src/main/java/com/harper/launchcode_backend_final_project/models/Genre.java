f6d7ee02b6357efc4e265190d77cc8656e49184d
Harper <HarperMelissaM@gmail.com>
2025-07-10 16:21:41 -0500

    add genre model

---
+++ src/main/java/com/harper/launchcode_backend_final_project/models/Genre.java
@@ -0,0 +1,26 @@
+package com.harper.launchcode_backend_final_project.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.harper.launchcode_backend_final_project.models.Movie;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Genre {
+    @Id
+    private int id;
+    private String name;
+    @ManyToMany
+    @JsonManagedReference
+    private List<Movie> movies;
+
+}
