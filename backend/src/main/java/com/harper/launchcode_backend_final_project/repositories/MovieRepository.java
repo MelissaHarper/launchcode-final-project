5da0650974e8afbd349f9ae09e435ce6b5aec701
Harper <HarperMelissaM@gmail.com>
2025-07-16 14:37:59 -0500

    add Movie repository

---
+++ backend/src/main/java/com/harper/launchcode_backend_final_project/repositories/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.harper.launchcode_backend_final_project.repositories;
+
+import com.harper.launchcode_backend_final_project.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
