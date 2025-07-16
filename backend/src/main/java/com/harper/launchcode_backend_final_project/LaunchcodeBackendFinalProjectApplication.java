ffb28bd8dad2a7463c441a078a28dfbc0ac59583
Harper <HarperMelissaM@gmail.com>
2025-07-16 14:40:55 -0500

    add project entry point

---
+++ backend/src/main/java/com/harper/launchcode_backend_final_project/LaunchcodeBackendFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.harper.launchcode_backend_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchcodeBackendFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchcodeBackendFinalProjectApplication.class, args);
+	}
+
+}
