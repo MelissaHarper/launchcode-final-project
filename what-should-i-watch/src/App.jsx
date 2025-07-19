45c04c63490ef51f168b7244598a006dea69f07a
Harper <HarperMelissaM@gmail.com>
2025-07-19 11:40:23 -0500

    move clerkprovider to app.js

--- what-should-i-watch/src/App.jsx
+++ what-should-i-watch/src/App.jsx
@@ -11,6 +11,7 @@ import Feedback from "./components/Feedback";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import movies from "./assets/data/mergedDummyData.json";
+import { ClerkProvider } from "@clerk/react-router";
 
 function App() {
   const [movieList, setMovieList] = useState();
@@ -26,38 +27,50 @@ function App() {
   useEffect(() => {
     setMovies();
   }, []);
+  // Import clerk Publishable Key
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+  // Throw error if key not imported
+  if (!PUBLISHABLE_KEY) {
+    throw new Error("Add your Clerk Publishable Key to the .env file");
+  }
 
   return (
     <main>
       <div className="App">
         <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route
-              path="/recommendations"
-              element={
-                <Recommendations
-                  movieList={movieList}
-                  setMovieList={setMovieList}
-                />
-              }
-            />
-            <Route
-              path="/filterContainer"
-              element={
-                <FilterContainer
-                  movieList={movieList}
-                  setMovieList={setMovieList}
-                />
-              }
-            />
-            <Route path="/movieCard" element={<MovieCard />} />
-            <Route path="/feedback" element={<Feedback />} />
-            <Route path="/selection/:type/detail/:id" element={<Selection />} />
-          </Routes>
-          <Footer />
+          <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+            <NavBar />
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/recommendations"
+                element={
+                  <Recommendations
+                    movieList={movieList}
+                    setMovieList={setMovieList}
+                  />
+                }
+              />
+              <Route
+                path="/filterContainer"
+                element={
+                  <FilterContainer
+                    movieList={movieList}
+                    setMovieList={setMovieList}
+                  />
+                }
+              />
+              <Route path="/movieCard" element={<MovieCard />} />
+              <Route path="/feedback" element={<Feedback />} />
+              <Route
+                path="/selection/:type/detail/:id"
+                element={<Selection />}
+              />
+            </Routes>
+            <Footer />
+          </ClerkProvider>
         </BrowserRouter>
       </div>
       <></>
--- what-should-i-watch/src/main.jsx
+++ what-should-i-watch/src/main.jsx
@@ -3,20 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ClerkProvider } from "@clerk/react-router";
-
-// Import clerk Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-// Throw error if key not imported
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk Publishable Key to the .env file");
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <App />
   </StrictMode>
 );
