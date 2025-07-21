67fef2b4f7f17113ee54c6705e1f19f13fe878b2
Harper <HarperMelissaM@gmail.com>
2025-07-21 15:14:20 -0500

    add saveList method

--- backend/src/main/java/com/harper/launchcode_backend_final_project/controllers/ToWatchListController.java
+++ backend/src/main/java/com/harper/launchcode_backend_final_project/controllers/ToWatchListController.java
@@ -1,4 +1,22 @@
 package com.harper.launchcode_backend_final_project.controllers;
 
+import com.harper.launchcode_backend_final_project.models.ToWatchList;
+import com.harper.launchcode_backend_final_project.services.ToWatchListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/to-watch-lists")
 public class ToWatchListController {
+    private final ToWatchListService toWatchListService;
+
+    @PostMapping
+    public ResponseEntity<ToWatchList> saveList(ToWatchList toWatchList) {
+        ToWatchList savedList = toWatchListService.saveList(toWatchList);
+        return ResponseEntity.ok(savedList);
+    }
 }
