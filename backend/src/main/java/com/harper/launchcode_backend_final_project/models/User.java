742c2f1a932c10e23f2737e69b98977197e95d2e
Harper <HarperMelissaM@gmail.com>
2025-07-23 09:14:46 -0500

    add clerkId, photourl, createdAt, and change relation to ToWatchList

--- backend/src/main/java/com/harper/launchcode_backend_final_project/models/User.java
+++ backend/src/main/java/com/harper/launchcode_backend_final_project/models/User.java
@@ -1,9 +1,15 @@
 package com.harper.launchcode_backend_final_project.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -14,13 +20,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
+
+    private String clerkId;
     private String username;
-    private String password;
     private String email;
+    private String photoUrl;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "to_watch_list_id")
+    private ToWatchList toWatchList;
+
     private boolean isLoggedIn;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private Set<ToWatchList> toWatchLists = new HashSet<>();
+    @CreatedDate
+    private Instant createdAt;
+
 
 
 }
