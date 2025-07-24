a32361b52101536ad6094c88f85d4bc644110f69
Harper <HarperMelissaM@gmail.com>
2025-07-24 11:29:07 -0500

    add comment for  filtered api call reference

--- what-should-i-watch/src/shared/utils.js
+++ what-should-i-watch/src/shared/utils.js
@@ -1,6 +1,11 @@
 import { options } from "./call-headers.js";
 // import movies from "../assets/data/movies.json";
 
+//curl --request GET \
+//  --url 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=18%2C%2014&with_watch_providers=8%2C%20337&without_watch_providers=1825' \
+//  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWNiMjE1Yjk3NWEzYzJmMzc4NzQzZmI2OTgzOGZmMiIsIm5iZiI6MTc0NzY4NTY4OS42NjYsInN1YiI6IjY4MmI5MTM5MzVkOWE1NzQxYjViNWMwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a6YZBJxVaiFmqgcgJeDw5syp5s2MHqEAcYSI4Vh4hX4' \
+//  --header 'accept: application/json'
+
 export function getRandomNumber() {
   let randomNumber = Math.floor(Math.random() * 80000);
   return randomNumber;
