5fcf39bc72111e9057deebacb2f6e9bcefe89ffb
Harper <HarperMelissaM@gmail.com>
2025-07-24 11:25:01 -0500

    change filter function name

--- what-should-i-watch/src/shared/call-functions.js
+++ what-should-i-watch/src/shared/call-functions.js
@@ -1,6 +1,6 @@
 import { api } from "./call-headers";
 
-export const getByGenre = async (searchTerms, payload) => {
+export const getWithFilters = async (searchTerms, payload) => {
   const response = await api().get(
     `/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${searchTerms}`,
     {
