1e9fa50cca55671f210d983f39dbd28673609bf7
Harper <HarperMelissaM@gmail.com>
2025-07-28 20:19:44 -0500

    add webhook verification with temporary test

--- what-should-i-watch/src/shared/call-headers.js
+++ what-should-i-watch/src/shared/call-headers.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { verifyWebhook } from "@clerk/react-router/webhooks";
 
 const authorization = import.meta.env.VITE_AUTHORIZATION;
-const baseUrl = import.meta.env.VITE_BASE_TMDB_API_URL;
+const tmdbBaseUrl = import.meta.env.VITE_BASE_TMDB_API_URL;
+export const tmdbApiKey = import.meta.env.TMDB_API_KEY;
 
 export const options = {
   method: "GET",
@@ -13,9 +15,27 @@ export const options = {
 
 export const api = () =>
   axios.create({
-    baseURL: baseUrl,
+    baseURL: tmdbBaseUrl,
     headers: {
       accept: "application/json",
       authorization: authorization,
     },
   });
+
+export const action = async ({ request }) => {
+  try {
+    const evt = await verifyWebhook(request);
+
+    const { id } = evt.data;
+    const eventType = evt.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`
+    );
+    console.log("Webhook payload:", evt.data);
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+};
