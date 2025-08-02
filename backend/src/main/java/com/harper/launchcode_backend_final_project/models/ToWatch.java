bdcfc005f6b8e5f42b828728be591e65460d64d1
Harper <HarperMelissaM@gmail.com>
2025-08-02 08:33:03 -0500

    change required params and toWatch relationship

--- backend/src/main/java/com/harper/launchcode_backend_final_project/models/ToWatch.java
+++ backend/src/main/java/com/harper/launchcode_backend_final_project/models/ToWatch.java
@@ -3,7 +3,11 @@ package com.harper.launchcode_backend_final_project.models;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,19 +15,13 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class ToWatch {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private int id;
 
-    @ManyToOne
-    @JsonManagedReference
-    private User user;
-
-    @NonNull
-    private String clerkId;
+    private String userId;
 
     @ManyToMany(cascade = {CascadeType.ALL})
     @JoinTable(
@@ -31,4 +29,13 @@ public class ToWatch {
             joinColumns = @JoinColumn(name = "to_watch_id"),
             inverseJoinColumns = @JoinColumn(name = "movie_id"))
     private Set<Movie> movies = new HashSet<>();
+
+    @NonNull
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @NonNull
+    @UpdateTimestamp
+    private Instant updatedAt;
 }
--- backend/src/main/java/com/harper/launchcode_backend_final_project/models/User.java
+++ backend/src/main/java/com/harper/launchcode_backend_final_project/models/User.java
@@ -3,8 +3,11 @@ package com.harper.launchcode_backend_final_project.models;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,33 +15,28 @@ import java.util.List;
 @Entity
 @Data
 @AllArgsConstructor
-@RequiredArgsConstructor(staticName = "of")
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
-    private int Id;
+    private String id;
 
-    @NonNull
-    private String clerkId;
-
-    @NonNull
     private String email;
-
-    @NonNull
     private String username;
-
-    @NonNull
     private String photoUrl;
 
-    @OneToMany(mappedBy = "user")
-    @JsonBackReference
-    private List<ToWatch> movies = new ArrayList<>();
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "to_watch_id", referencedColumnName = "id")
+    private ToWatch toWatch;
 
-    @CreatedDate
     @NonNull
-    @Setter(AccessLevel.NONE)
+    @CreationTimestamp
+    @Column(updatable = false)
     private Instant createdAt;
 
+    @NonNull
+    @UpdateTimestamp
+    private Instant updated;
+
 }
+
