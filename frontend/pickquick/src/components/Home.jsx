cd16f7c921a49a2f8aeb02fa89d3c1f6f3b58dec
Harper <HarperMelissaM@gmail.com>
2025-08-15 18:08:30 -0500

    add unstyled instructions

--- frontend/pickquick/src/components/Home.jsx
+++ frontend/pickquick/src/components/Home.jsx
@@ -8,6 +8,26 @@ function Home() {
       <div className="hero-div">
         <img className="hero" src={hero} alt="PickQuick logo" />
       </div>
+      <div className="card-list">
+        <div className="card">
+          Our goal is to reduce decision fatigue by giving fast, relevant, and
+          fun recommendations without endlessly scrolling through streaming
+          platforms.
+        </div>
+        <div className="card">
+          Below, select the genres and streaming providers you'd like to include
+          in the results.
+        </div>
+        <div className="card">
+          Hit submit and we'll give you 5 movie choices.
+        </div>
+        <div className="card">
+          Click on a movie poster to get more information
+        </div>
+        <div className="card">
+          Use our secure sign-in and add movies to your personal watchlist
+        </div>
+      </div>
 
       <FilterContainer />
     </div>
