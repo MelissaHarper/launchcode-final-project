c67688dfd2eefd2336f6e00afb3913745b8f2fd6
Harper <HarperMelissaM@gmail.com>
2025-08-18 18:23:47 -0500

    style instructions

--- frontend/pickquick/src/components/Home.jsx
+++ frontend/pickquick/src/components/Home.jsx
@@ -1,31 +1,43 @@
+import { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import FilterContainer from "./FilterContainer";
 import hero from "../assets/images/PickQuickLogo.png";
 import "../App.css";
 
 function Home() {
+  const [active, setActive] = useState(1);
+
+  const handleActive = (num) => {
+    setActive(num);
+  };
+
   return (
     <div className="home">
       <div className="hero-div">
         <img className="hero" src={hero} alt="PickQuick logo" />
       </div>
       <div className="card-list">
-        <div className="card">
-          Our goal is to reduce decision fatigue by giving fast, relevant, and
-          fun recommendations without endlessly scrolling through streaming
-          platforms.
+        <div className={active === 1 ? "card-active" : "card-from-right"}>
+          <FiChevronLeft size={40} color="transparent" />
+          Stop endlessly scrolling through streaming platforms and get fast,
+          relevant recommendations.
+          <FiChevronRight size={40} onClick={() => handleActive(2)} />
         </div>
-        <div className="card">
+        <div className={active === 2 ? "card-active" : "card-from-right"}>
+          <FiChevronLeft size={40} onClick={() => handleActive(1)} />
           Below, select the genres and streaming providers you'd like to include
           in the results.
+          <FiChevronRight size={40} onClick={() => handleActive(3)} />
         </div>
-        <div className="card">
-          Hit submit and we'll give you 5 movie choices.
+        <div className={active === 3 ? "card-active" : "card-from-right"}>
+          <FiChevronLeft size={40} onClick={() => handleActive(2)} />
+          You can click on any movie poster to get more information
+          <FiChevronRight size={40} onClick={() => handleActive(4)} />
         </div>
-        <div className="card">
-          Click on a movie poster to get more information
-        </div>
-        <div className="card">
-          Use our secure sign-in and add movies to your personal watchlist
+        <div className={active === 4 ? "card-active" : "card-from-right"}>
+          <FiChevronLeft size={40} onClick={() => handleActive(3)} />
+          Sign-in to add movies to your personal watchlist
+          <FiChevronLeft color="transparent" size={40} />
         </div>
       </div>
 
