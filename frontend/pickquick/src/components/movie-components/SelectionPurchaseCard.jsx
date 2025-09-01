fd83004fe772f41eacb209c40d056b87aa0b0e11
Harper <HarperMelissaM@gmail.com>
2025-09-01 16:35:31 -0500

    add provider icons to selection page

---
+++ frontend/pickquick/src/components/movie-components/SelectionPurchaseCard.jsx
@@ -0,0 +1,44 @@
+import { tmdbImgBaseUrl } from "../services/call-headers.js";
+import "../../styles/Selection.css";
+
+const SelectionPurchases = ({ providers }) => {
+  return (
+    <div>
+      {/* Purchase Providers */}
+      {providers.results.US.rent && (
+        <div>
+          <p className="title">Rent</p>
+          <div className="purchase-provider-container">
+            {providers.results.US.rent.map((provider) => (
+              <div key={provider.provider_id} className="provider">
+                <img
+                  className="provider-image"
+                  src={`${tmdbImgBaseUrl}${provider.logo_path}`}
+                  alt={provider.provider_name}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {providers.results.US.buy && (
+        <div>
+          <p className="title">Buy</p>
+          <div className="purchase-provider-container">
+            {providers.results.US.buy.map((provider) => (
+              <div key={provider.provider_id} className="provider">
+                <img
+                  className="provider-image"
+                  src={`${tmdbImgBaseUrl}${provider.logo_path}`}
+                  alt={provider.provider_name}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectionPurchases;
