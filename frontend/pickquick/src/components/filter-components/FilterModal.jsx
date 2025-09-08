006130b70f0a4c287f065585b820803ad3bbaf92
Harper <HarperMelissaM@gmail.com>
2025-09-08 15:31:18 -0500

    change to modal add keyword logic

---
+++ frontend/pickquick/src/components/filter-components/FilterModal.jsx
@@ -0,0 +1,263 @@
+import {
+  createContext,
+  useContext,
+  useRef,
+  useState,
+  useEffect,
+  Fragment,
+} from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Listbox,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { FiCheck, FiChevronDown, FiSearch } from "react-icons/fi";
+import { FaXmark } from "react-icons/fa6";
+import KeywordSearch from "./KeywordSearch";
+import { tmdbImgBaseUrl } from "../services/call-headers";
+
+const FilterContext = createContext();
+const FilterDropdown = ({
+  children,
+  setMovieList,
+  assignedList,
+  setAssignedList,
+  options,
+  selectionIdentifier,
+}) => {
+  const FilterDropdownRef = useRef(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <FilterContext.Provider
+      value={{
+        setMovieList,
+        assignedList,
+        setAssignedList,
+        options,
+        selectionIdentifier,
+        FilterDropdownRef,
+        isDropdownOpen,
+        setIsDropdownOpen,
+      }}
+    >
+      <div ref={FilterDropdownRef}>{children}</div>
+    </FilterContext.Provider>
+  );
+};
+
+const Button = () => {
+  const { setIsDropdownOpen } = useContext(FilterContext);
+  return (
+    <button
+      className="  px-4 py-2 flex items-center justify-between w-full rounded border border-[#828FA340] hover:border-primary cursor-pointer relative text-[#605e80]"
+      onClick={() => setIsDropdownOpen(true)}
+    >
+      <span className="block">
+        <FiChevronDown color="white" size={24} />
+      </span>
+
+      <FilterDropdown.List />
+    </button>
+  );
+};
+
+const Header = () => {
+  return <label className="mt-4 mb-2 text-sm"></label>;
+};
+
+const ListContainer = () => {
+  const { options, isDropdownOpen, setIsDropdownOpen } =
+    useContext(FilterContext);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    // Filter options based on searchTerm
+    {
+      options &&
+        setFilteredOptions(
+          options.filter((option) =>
+            option.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        );
+    }
+  }, [searchTerm, options]);
+
+  return (
+    isDropdownOpen && (
+      <ul
+        className={`absolute bottom-full -translate-x-5 text-white rounded border border-[#828FA340] translate-y-full rounded bg-[#20212c] w-full list-none z-1`}
+      >
+        <Transition appear show={isDropdownOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-[999999]"
+            onClose={() => {
+              setIsDropdownOpen(false);
+            }}
+            ref={dropdownRef}
+          >
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black/50 backdrop-blur" />
+            </TransitionChild>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <TransitionChild
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <DialogPanel className="w-full max-w-md p-4 text-left align-middle rounded-xl bg-background-light dark:bg-slate-900 shadow-xl transform overflow-hidden transition-all">
+                    {options ? (
+                      <div className="relative w-full">
+                        <Listbox multiple>
+                          <FilterDropdown.Close />
+                          <FiSearch />
+                          <input
+                            type="text"
+                            placeholder="Search..."
+                            className="search-input"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                          />
+                          {filteredOptions?.map((option, index) => (
+                            <FilterDropdown.Item key={index} option={option} />
+                          ))}
+                        </Listbox>
+                      </div>
+                    ) : (
+                      <KeywordSearch />
+                    )}
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+      </ul>
+    )
+  );
+};
+
+const Item = ({ option }) => {
+  const { assignedList, setAssignedList } = useContext(FilterContext);
+
+  function handleAssign(option) {
+    setAssignedList((prevList) => {
+      if (prevList.some((item) => item.id === option.id)) {
+        return prevList.filter((item) => item.id !== option.id);
+      } else {
+        return [...prevList, option];
+      }
+    });
+  }
+
+  return (
+    <li
+      key={option.id}
+      className="flex items-center gap-2 p-4 hover:bg-[#2b2c37] rounded transition-all duration-200 cursor-pointer"
+      onClick={() => handleAssign(option)}
+    >
+      {assignedList.some((item) => item.id === option.id) && <FiCheck />}
+
+      {option.imgUrl && (
+        <img
+          className="w-6 h-6"
+          src={`${tmdbImgBaseUrl}${option.imgUrl}`}
+          alt={`${option.name} image`}
+        />
+      )}
+      <span>{option.name}</span>
+    </li>
+  );
+};
+
+const AssignedList = () => {
+  const { assignedList, setAssignedList, selectionIdentifier } =
+    useContext(FilterContext);
+
+  function handleRemove(id) {
+    setAssignedList((assignedList) =>
+      assignedList.filter((option) => option.id !== id)
+    );
+  }
+
+  if (assignedList.length === 0)
+    return (
+      <p className="mt-4 p-2 shadow-sm bg-[#272727] rounded">
+        No options selected.
+      </p>
+    );
+
+  return (
+    <div className="mt-4 p-2 shadow-sm bg-[#272727] rounded">
+      <h2 className="px-2 my-3 font-bold">{`Selected ${selectionIdentifier}: `}</h2>
+      <div className="flex flex-wrap gap-4 ">
+        {assignedList?.map((option) => (
+          <div
+            key={option.id}
+            className="flex items-center gap-1 w-[47.5%] p-2 hover:bg-[#20212c] rounded transition-all duration-200"
+            onClick={() => handleRemove(option.id)}
+          >
+            {option.imgUrl && (
+              <img
+                className="w-6 h-6 "
+                src={`${tmdbImgBaseUrl}${option.imgUrl}`}
+                alt={`${option.name} image`}
+              />
+            )}
+
+            <span>{option.name}</span>
+            <span className="ml-auto cursor-pointer p-1 hover:bg-[#2b2c37] rounded-full">
+              <FaXmark />
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Close = () => {
+  const { setIsDropdownOpen } = useContext(FilterContext);
+  return (
+    <div
+      className="absolute top-5 right-0 flex items-center justify-center -translate-y-full gap-2 bg-[#C0392B] px-2 py-1 rounded-t"
+      onClick={(e) => {
+        e.stopPropagation();
+        setIsDropdownOpen(false);
+      }}
+    >
+      <span>Close</span>
+      <span>
+        <FaXmark size={20} />
+      </span>
+    </div>
+  );
+};
+
+export default FilterDropdown;
+
+FilterDropdown.List = ListContainer;
+FilterDropdown.Item = Item;
+FilterDropdown.Header = Header;
+FilterDropdown.Button = Button;
+FilterDropdown.AssignedList = AssignedList;
+FilterDropdown.Close = Close;
